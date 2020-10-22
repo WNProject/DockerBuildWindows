ada402c6e42bd2634282e9968cc359b13538d2dd
Devon Powell <devon.f.powell@gmail.com>
2020-10-22 08:58:44 -0400

    Add debug option
    If you set DEBUG=1 when running the docker container it will spit out a bunch of
    info about the setup of Visual Studio.

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,25 +1,41 @@
 $version = $env:VS_VERSION
 $architecture = $env:VS_ARCH
-
-Write-Host 'Setup Visual C++ Build Environment ...'
-Write-Host "  Version: $version"
-Write-Host "  Architecture: $architecture"
-
+$debug = $env:DEBUG
 $versionHigh = $version + 1
 $range = "[$version.0,$versionHigh.0)"
 $vs = Get-VSSetupInstance | Select-VSSetupInstance -Version $range -Product *
 
 if (!$vs) {
+  Write-Host 'Unable to get installed Visual Studio info'
+
   exit 1
 }
 
 $vsPath = $vs.InstallationPath
 
 if (!$vsPath) {
+  Write-Host 'Unable to get installed Visual Studio path'
+
   exit 1
 }
 
 $vcvars = Join-Path $vsPath 'VC/Auxiliary/Build/vcvarsall.bat'
+
+if (!(Test-Path $vcvars)) {
+  Write-Host 'Expected path for Visual Studio vsvarsall does not exist'
+
+  exit 1
+}
+
+if ($debug -eq 1) {
+  Write-Host 'Setup Visual C++ Build Environment ...'
+  Write-Host "  Version: $version"
+  Write-Host "  Full Version: $($vs.InstallationVersion)"
+  Write-Host "  Architecture: $architecture"
+  Write-Host "  Name: $($vs.DisplayName)"
+  Write-Host "  Path: $vsPath"
+  Write-Host "  VCVars Path: $vcvars"
+}
 
 cmd /c """$vcvars"" $architecture & set" | foreach {
   if ($_ -Match "=") {
@@ -29,8 +45,11 @@ cmd /c """$vcvars"" $architecture & set" | foreach {
   }
 }
 
-Write-Host 'Done'
-Write-Host '------------------------------------------------------------'
+if ($debug -eq 1) {
+  Write-Host 'Done'
+  Write-Host '------------------------------------------------------------'
+}
+
 Invoke-Expression "$args"
 
 exit $LastExitCode
